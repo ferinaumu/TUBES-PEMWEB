6ea50c99327d6ae256b68018071d6ea24b370a99
ferinaumu <ferinaumusalamah@student.uns.ac.id>
2017-06-18 19:32:39 +0700

    v1.0.0

---
+++ admin_toko.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title></title>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+    <link href="css/style.css" rel="stylesheet" type="text/css" media="all"/>
+    <script type="text/javascript" src="js/jquery-1.7.2.min.js"></script>
+    <script type="text/javascript" src="js/move-top.js"></script>
+    <script type="text/javascript" src="js/easing.js"></script>
+  </head>
+
+  <body>
+    <div class="wrap">
+      <div class="header">
+        <div class="header_top">
+    			<div class="logo">
+    				<a href="index.html"><img src="images/logo.png" alt="" /></a>
+    			</div>
+      	  <div class="clear"></div>
+        </div>
+      </div>
+      <div class="header_bottom">
+  	     	<div class="menu">
+  	     		<ul>
+  			    	<li><a href="index.php">Home</a></li>
+  			    	<li><a href="admin_user.php">User</a></li>
+  			    	<li class="active"><a href="admin_toko.php">Toko</a></li>
+  			    	<li><a href="admin_pengaduan.php">Pengaduan</a></li>
+  			    	<li><a href="admin_transaksi.php">Transaksi</a></li>
+  			    	<div class="clear"></div>
+       			</ul>
+  	     	</div>
+  	     	<div class="search_box">
+  	     		<form>
+  	     			<input type="text" value="Search" onfocus="this.value = '';" onblur="if (this.value == '') {this.value = 'Search';}"><input type="submit" value="">
+  	     		</form>
+  	     	</div>
+  	     	<div class="clear"></div>
+  	     </div>
+       </div>
+    </div>
+  </body>
+</html>
